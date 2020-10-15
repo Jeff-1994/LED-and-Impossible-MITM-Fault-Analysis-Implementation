076b897df6d3d3c83e12d8cee33ac392d4f566bd
jeff-1994 <jafarmohammadi@outlook.com>
2020-10-15 15:31:49 +0330

    LED Block Cipher Enc/Dec Implementation Modified at 2020/15/Oct ...!

--- Program.cs
+++ Program.cs
@@ -13,29 +13,29 @@ namespace LED_Block_Cipher
             // Convert Key And PlainText To Program's Format : 
             int[,] plain = 
                 { 
-                    { 0, 1, 2, 3 },
-                    { 4, 5, 6, 7 },
-                    { 8, 9, 10, 11 },
+                    { 0,  1,  2,  3  },
+                    { 4,  5,  6,  7  },
+                    { 8,  9,  10, 11 },
                     { 12, 13, 14, 15 }
                 };
             Block plainText = new Block(plain);
 
             int[,] k =
                 {
-                    { 0, 1, 2, 3 },
-                    { 4, 5, 6, 7 },
-                    { 8, 9, 10, 11 },
+                    { 0,  1,  2,  3  },
+                    { 4,  5,  6,  7  },
+                    { 8,  9,  10, 11 },
                     { 12, 13, 14, 15 }
                 };
             Block key = new Block(k);
             
+            // Construct a LED object and execute Inc/Dec Method : 
             LED led = new LED();
             led.encryption(plainText, key);
 
+            // Construct an IMFA object and execute the analysis : 
             // IMFA analysis = new IMFA();
             // analysis.execute();
-
-            // Console.ReadLine();
         }
     }
 }
